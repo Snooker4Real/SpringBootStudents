20b8b41d55361360361ddfd902739b338b02f878
Cindano Jonathan <65620947+Snooker4Real@users.noreply.github.com>
2021-07-26 05:38:45 +0200

    modified

--- src/main/java/fr/snooker4real/service/StudentService.java
+++ src/main/java/fr/snooker4real/service/StudentService.java
@@ -13,7 +13,7 @@ import java.util.Collection;
 public class StudentService {
 
     @Autowired
-    @Qualifier("mongoData")
+    @Qualifier("fakeData")
     private StudentDao studentDao;
 
     public Collection<Student> getAllStudents(){
